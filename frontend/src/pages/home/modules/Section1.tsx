2dacb35c2d4cacafce1bfa3890797cdf0fa1f4af
DuskStarGITHUB <contactspeencer@gmail.com>
2025-09-26 13:48:41 -0600

    style:responsive

--- frontend/src/pages/home/modules/Section1.tsx
+++ frontend/src/pages/home/modules/Section1.tsx
@@ -24,26 +24,26 @@ const Section1: React.FC<Section1Props> = ({ t }) => {
   return (
     <section className="relative h-screen w-full flex flex-col justify-center p-6">
       <div className="grid grid-cols-2">
-        <div className="relative text-left p-6 max-w-3xl left-0 md:left-1/12 2xl:left-2/10">
-          <h1 className="mb-4 text-6xl font-[Montserrat] font-medium">
+        <div className="relative text-left p-4 sm:p-6 max-w-full sm:max-w-3xl left-0 sm:left-1/12 2xl:left-2/10">
+          <h1 className="mb-4 text-3xl sm:text-4xl md:text-6xl font-[Montserrat] font-medium">
             {t("home.text-1")}
           </h1>
-          <p className="mb-8 text-4xl font-light">{t("home.text-2")}</p>
-          <div className="flex flex-col gap-4">
-            <div className="flex gap-4">
-              <button
-                className="rounded bg-accent px-4 py-2 font-medium text-white dark:bg-gray-950 hover:cursor-pointer hover:dark:bg-black"
-                onClick={() => navigate("/login")}
-              >
-                {t("words.login")}
-              </button>
-              <button
-                className="rounded border-2 px-4 py-2 font-medium border-white hover:cursor-pointer hover:border-gray-800"
-                onClick={() => navigate("/register")}
-              >
-                {t("words.register")}
-              </button>
-            </div>
+          <p className="mb-8 text-xl sm:text-2xl md:text-4xl font-light">
+            {t("home.text-2")}
+          </p>
+          <div className="flex flex-col gap-4 sm:flex-row sm:gap-4">
+            <button
+              className="w-full sm:w-auto rounded bg-accent px-4 py-2 font-medium text-white dark:bg-gray-950 hover:cursor-pointer hover:dark:bg-black"
+              onClick={() => navigate("/login")}
+            >
+              {t("words.login")}
+            </button>
+            <button
+              className="w-full sm:w-auto rounded border-2 px-4 py-2 font-medium border-white hover:cursor-pointer hover:border-gray-800"
+              onClick={() => navigate("/register")}
+            >
+              {t("words.register")}
+            </button>
           </div>
         </div>
         <div className="flex items-center justify-center">
